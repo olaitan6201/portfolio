2cb1e594b80f99b7c0122147d02c92697426d456
Olaitan Habeeb Olamide <olaitan6201@gmail.com>
2021-08-31 11:09:29 +0100

    completed

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,27 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+function send_mail($from='', $to='', $subject='', $content='', $cc=''){
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= 'From: ' .$from. "\r\n";
+  $headers .= 'Cc: '.$cc. "\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  $mail = mail($to, $subject, $content, $headers);
+  return $mail;
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// extract($_POST);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$from = htmlspecialchars($_POST['fr']);
+// $to = htmlspecialchars($_POST['t']);
+$subject = htmlspecialchars($_POST['sub']);
+$txt = htmlspecialchars($_POST['text']);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$content = 'Message From: '.$_POST['name']."\r\n";
+$content .= $txt;
 
-  echo $contact->send();
+send_mail($from, 'olaitan6201@gmail.com', $subject, $content, '');
+send_mail($from, 'olaitan.habeeb.001@gmail.com', $subject, $content, '');
+
+// header('location: ../');
 ?>
